49364e88ea3d9a5fcba3d5104e267456395fd7e3
javisd3 <javier.smh06@gmail.com>
2025-08-01 21:13:17 +0200

    gj

---
+++ Simulacion_Prueba/probabilidad/probabilidad_dado.py
@@ -0,0 +1,10 @@
+# probabilidad_dado.py
+# Calcula la probabilidad de sacar un 6 al lanzar un dado
+
+def probabilidad_seis():
+    lados = 6
+    probabilidad = 1 / lados
+    print(f"La probabilidad de sacar un 6 al lanzar un dado es: {probabilidad}")
+
+if __name__ == "__main__":
+    probabilidad_seis()
